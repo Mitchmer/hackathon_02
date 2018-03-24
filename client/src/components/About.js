c94743b0ecd9676a917df76380d0cb9aad5c143b
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-03-24 12:08:50 -0600

    about us mostly done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -14,23 +14,26 @@ class About extends Component {
     return (
       <div>
         <Container class="ui container" >
-          <Header as="h2" textAlign="center">About Us</Header>
+          <br/>
+          <Header as="h1" textAlign="center">About Us</Header>
         </Container>
         <Segment>
           <Header as="h4" textAlign="Left">Basic Info</Header>
-          <div >
-          565 E 2100 S
-          Salt Lake City, UT 84106
-          (801) 268-3374
-          Open: Wednesday - Sunday 11am - 9pm
-          Closed: Monday & Tuesday
-          </div>
-          <a href="https://www.google.com/maps/place/565+Firehouse/@40.7256923,-111.8769849,17z/data=!3m1!4b1!4m5!3m4!1s0x87528ab9882e4d53:0xf67f854b36163cf6!8m2!3d40.7256923!4d-111.8747962" >
-          Directions
-          </a>
-        </Segment>
-        <Segment>
-          <Header as="h4">Contact Us</Header>
+            <div >
+              565 E 2100 S 
+            <br/>
+              Salt Lake City, UT 84106
+            <br/>
+              (801) 268-3374
+            <br/>
+              Open: Wednesday - Sunday 11am - 9pm
+            <br/>
+              Closed: Monday & Tuesday
+            </div>
+              <br/>
+              <a href="https://www.google.com/maps/place/565+Firehouse/@40.7256923,-111.8769849,17z/data=!3m1!4b1!4m5!3m4!1s0x87528ab9882e4d53:0xf67f854b36163cf6!8m2!3d40.7256923!4d-111.8747962" >
+                Directions
+              </a>
         </Segment>
         <Segment>
           <Header as="h4">Upcoming Events</Header>
@@ -38,7 +41,10 @@ class About extends Component {
         <Segment>
           <Header as="h4">Follow Us</Header>
           <a href="https://twitter.com/BubbasFirehouse">
-            <img src="../images/twitter.png" alt="twitter" width="100" height="100"/>
+            <img src="../images/twitter.png" alt="twitter" width="50" height="50"/>
+          </a>
+          <a href="https://www.yelp.com/search?find_desc=bubba%27s+Firehouse+BBQ&find_loc=Highland%2C+UT&ns=1">
+            <img src="../images/yelp.png" alt="yelp" width="50" height="50" />
           </a>
         </Segment>
       </div>
