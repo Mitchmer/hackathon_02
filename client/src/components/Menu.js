8c1193a7c2eb9eea19c184168e1e706f84a8862f
Andrew Street <andrew_street@live.com>
2018-03-24 15:53:11 -0600

    cart

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TableBody, { Header, Container, Table, Image, Button, Segment, Divider } from 'semantic-ui-react';
+import TableBody, { Header, Container, Table, Image, Button, Segment, Divider} from 'semantic-ui-react';
 import Order from './Order';
 import { getItems } from '../actions/items';
 
 class Menu extends React.Component {
-  state = { name: '', price: 0 }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.additem(this.state.name)
-    this.setState({ name: '', price: ''})
-  }
+  state = { cart: [] }
 
   componentDidMount() {
     this.props.dispatch(getItems())
   }
+
+  addItemToCart = (item) => {
+    console.log(this.state.cart)
+    this.setState({ cart: [ item, ...this.state.cart ]})
+  }
+
 
   
   items = () => {
@@ -38,11 +38,11 @@ class Menu extends React.Component {
           </Table.Header>
         
           {items.map( item =>
-            <Table.Body>
+            <Table.Body key={item.id}>
               <Table.Row>
                 <Table.Cell>{item.name}</Table.Cell>
                 <Table.Cell>${item.price}</Table.Cell>
-                <Table.Cell><Button value="submit" color="green">Add To Cart</Button></Table.Cell>
+                <Table.Cell><Button onClick={() => this.addItemToCart(item)} value="submit" color="green">Add To Cart</Button></Table.Cell>
               </Table.Row>
             </Table.Body>
           )}
